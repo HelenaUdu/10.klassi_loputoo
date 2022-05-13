a932ea867c2550fda06b4db6e484f5e32f36bf08
HCU <helena.carmenu@gmail.com>
2022-05-13 12:03:23 +0300

    töötab:

--- test.py
+++ test.py
@@ -7,10 +7,25 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.stacklayout import StackLayout
 
 
 class MyGrid(Widget):
+    """
+    nimi = ObjectProperty(None)
+    kirjeldus = ObjectProperty(None)
+    aasta = ObjectProperty(None)
+    kuu = ObjectProperty(None)
+    paev = ObjectProperty(None)
+
+    def lisanupp(self):
+        pass
+"""
+
+class Telg(Widget):
     pass
+
 
 class MyApp(App):
     def build(self):
