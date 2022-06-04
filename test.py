8131070993156c46d7cee374e0a94fd0f0c5ad62
Urmas <urmas.akerman@real.edu.ee>
2022-06-04 11:37:05 +0300

    asjad lisanupp-ust panin lisa_eventi(), aga add_widget pidi jääma, nüüd teeb errori puhul hüpikakna

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-"""
-[] restrictida textinput väljad(numbrid vahemikus 1-12 kuul, päevadel 1-31, aastal number)
-"""
-
-
-
-
-
-
-
 from cgitb import text
 from msilib.schema import CheckBox
+from tabnanny import check
+from idna import check_initial_combiner
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -23,39 +15,11 @@ from kivy.core.window import Window
 from kivy.uix.popup import Popup
 import json
 
-import json
-
 class MyGrid(Widget):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-    
 
-    def lisanupp(self):
-        # Paneme pärast need lisanupp ja lisa_event kokku
-        # võtab myfile.jsonist kuupaevad(formaat aasta+kuu+päev nt 18840117) ja sordib need bubblesort algoritmiga
-        # Teised andmed nimi, kirjeldus jne võiksid olla listis, mille sees on dictionaryd või teised listid, leppime tunnis kokku
-        def bubblesort(järjend, pikkus):
-            for i in range(pikkus):
-                for j in range(pikkus -1):
-                    if järjend[j] > järjend[j+1]:
-                        järjend[j], järjend[j+1] = järjend[j+1], järjend[j]
-         
-            return järjend
-        
-        # võtab myfile.jsonist kuupaevad(formaat aasta+kuu+päev nt 18840117) ja sordib need bubblesort algoritmiga
-        kuupaevad = []
-
-        fhand = open(MyApp.filename)
-        data = json.load(fhand)
-        for event in data:
-            kuupaev = int(data[event]['aasta'] + data[event]['kuu'] + data[event]['paev'])
-            kuupaevad.append(kuupaev)
-
-        
-        fhand.close()
-        pikkus = len(kuupaevad)
-        bubblesort(kuupaevad, pikkus)
-
+    def lisanupp(self, kuupaevad, pikkus):
         n = 1 # mitmes event on ajateljel
         for i in kuupaevad:
             x = Window.width/pikkus*n
@@ -64,30 +28,54 @@ class MyGrid(Widget):
             self.add_widget(button)
             # self.ids[sõnastik[nimi]] = button
             n += 1
-            
-
+    
 
 class MyApp(App):
     def build(self):
         return MyGrid()
-    
-    filename = 'myfile.json'
 
+    def __init__(self, **kwargs):
+        super(MyApp, self).__init__(**kwargs)
+        Window.bind(on_request_close=self.on_request_close)
+
+    filename = 'myfile.json'
     lubatudpaevad = []
     lubatudkuud = []
 
+    # kui programm käivitub, seda koodi jooksutatakse
     def on_start(self): 
         self.read_data('lastevent')
-
-
     
-    def on_stop(self):
-        if self.piira_kuupaev() == 'VIGA':
-            return
-        self.save_data()
+    # checkib, et kuupäevad on õiged ja evendi nimed ei korduks
+    # kui return False -> kõik korras
+    # kui return True -> mingi jama, errori akna ette
+    def check_for_error(self):
+        evendid = []
+        fhand = open(self.filename)
+
+        data = json.load(fhand)
+        for event in data:
+            evendid.append(event)
+        fhand.close()
+        event = self.root.ids.nimi.text
+
+        if event in evendid[:-1]:
+            popup = Popup(title='VIGA', content=Label(text='Sama nimega event on olemas juba'), size_hint=(None, None), size=(400, 400))
+            popup.open()
+            return True
+        elif self.piira_kuupaeva() == 'VIGA':
+            return True
+        else:
+            self.save_data()
+            return False
+
+    # kui programmi tahetakse kinni panna, siis checkib errorite jäoks
+    # kui error on, siis ei lase kinni panna programmi
+    def on_request_close(self, *largs, **kwargs):
+        return self.check_for_error()
 
     # piirab kuude ja päevade sisestuse. Ei saa enam panna kuupäevade väljadessa tähti (nt: "200abcd") - siis viskab errori
-    def piira_kuupaev(self):
+    def piira_kuupaeva(self):
         for num in range(1, 32):
             if num < 10:
                 self.lubatudpaevad.append('0' + str(num))
@@ -112,8 +100,6 @@ class MyApp(App):
             popup.open()
             return 'VIGA'
 
-        
-
     # kustutab praeguse evendi
     def kustuta_event(self):
         fhand = open(self.filename)
@@ -126,24 +112,41 @@ class MyApp(App):
         self.read_data('lastevent')
 
     # lisab uue eventi
+    # võtab myfile.jsonist kuupaevad(formaat aasta+kuu+päev nt 18840117) ja sordib need bubblesort algoritmiga
+    # Teised andmed nimi, kirjeldus jne võiksid olla listis, mille sees on dictionaryd või teised listid, leppime tunnis kokku
     def lisa_event(self):
-        evendiloend = []
-        if self.piira_kuupaev() == 'VIGA':
+        if self.check_for_error() == True:
             return
+        evendiloend = []
+        kuupaevad = []
         self.save_data()
         fhand = open(self.filename)
         data = json.load(fhand)
         for event in data:
             evendiloend.append(event)
-        event = 'template' + str(len(evendiloend))
+            kuupaev = int(data[event]['aasta'] + data[event]['kuu'] + data[event]['paev'])
+            kuupaevad.append(kuupaev)
+        event = 'uusevent' + str(len(evendiloend))
         data[event] = data['template']
         fhand.close()
+
         fhand = open(self.filename, 'w')
         json.dump(data, fhand, indent=2)
         fhand.close()
-        self.read_data(event)
-        MyGrid().lisanupp()
 
+        def bubblesort(järjend, pikkus):
+            for i in range(pikkus):
+                for j in range(pikkus -1):
+                    if järjend[j] > järjend[j+1]:
+                        järjend[j], järjend[j+1] = järjend[j+1], järjend[j]
+         
+            return järjend
+    
+        pikkus = len(kuupaevad)
+        bubblesort(kuupaevad, pikkus)
+
+        self.read_data(event)
+        MyGrid().lisanupp(kuupaevad, pikkus)
 
     # kirjutab andmeid json faili, uuendab praeguse ajatelje nime
     def save_data(self):
@@ -162,9 +165,6 @@ class MyApp(App):
         fhand.close()
         self.currentevent = event
 
-
-
-
     # Loeb andmeid json failist
     def read_data(self, event):
         fhand = open(self.filename)
@@ -172,7 +172,6 @@ class MyApp(App):
         if event == 'lastevent':
             for i in data:
                 event = i 
-
         self.root.ids.nimi.text = event
         self.root.ids.aasta.text = data[event]['aasta']
         self.root.ids.ekr.active = data[event]['ekr']
@@ -181,7 +180,6 @@ class MyApp(App):
         self.root.ids.kirjeldus.text = data[event]['kirjeldus']
         self.root.ids.varv.color = data[event]['varv']
         self.currentevent = event
-
 
 
 if __name__ == '__main__':
